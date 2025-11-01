d7c8b2a98a4562b5491b0572137b5678196f5bac
shishirui <shirui@gmail.com>
2025-11-01 18:45:45 +0800

    Initial Commit

---
+++ SRNotificationView/SRNotificationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRNotificationViewApp.swift
+//  SRNotificationView
+//
+//  Created by rexshi on 2025/11/1.
+//
+
+import SwiftUI
+
+@main
+struct SRNotificationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
